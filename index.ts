63ab7c35eea7dd34bf66ee849c637cd9eab6b15b
Amrit kumar Mahto <amrit.mahto@adypu.edu.in>
2026-03-04 23:24:41 +0530

    api part

--- index.ts
+++ index.ts
@@ -4,65 +4,109 @@ const { Command } = require("commander")
 const program = new Command()
 
 program
-.command("greet <name>")
-.action((name)=>{
-    console.log(`Hello ${name}`)
-})
+    .command("greet <name>")
+    .action((name) => {
+        console.log(`Hello ${name}`)
+    })
 program
-.command("add <a> <b>")
-.action((a,b)=>{
-    console.log(Number(a) + Number(b))
-})
+    .command("add <a> <b>")
+    .action((a, b) => {
+        console.log(Number(a) + Number(b))
+    })
 program
-.command("sub <a> <b>")
-.action((a,b)=>{
-    console.log(Number(a) - Number(b))
-})
+    .command("sub <a> <b>")
+    .action((a, b) => {
+        console.log(Number(a) - Number(b))
+    })
 program
-.command("multi <a> <b>")
-.action((a,b)=>{
-    console.log(Number(a) * Number(b))
-})
+    .command("multi <a> <b>")
+    .action((a, b) => {
+        console.log(Number(a) * Number(b))
+    })
 program
-.command("div <a> <b>")
-.action((a,b)=>{
-    if (Number(b)!==0){
-    console.log(Number(a) / Number(b))}
-    else{
-        console.log("not divisible")
+    .command("div <a> <b>")
+    .action((a, b) => {
+        if (Number(b) !== 0) {
+            console.log(Number(a) / Number(b))
+        }
+        else {
+            console.log("not divisible")
+        }
+    })
+program
+    .command("mod <a> <b>")
+    .action((a, b) => {
+
+        console.log(Number(a) % Number(b))
     }
-})
+    )
 program
-.command("mod <a> <b>")
-.action((a,b)=>{
+    .command("floor <a>")
+    .action((a) => {
 
-    console.log(Number(a) % Number(b))}
- )
+        console.log(Math.floor(Number(a)))
+    }
+    )
 program
-.command("floor <a>")
-.action((a)=>{
+    .command("ceil <a>")
+    .action((a) => {
 
-    console.log(Math.floor(Number(a)))}
- )
+        console.log(Math.ceil(Number(a)))
+    }
+    )
 program
-.command("ceil <a>")
-.action((a)=>{
+    .command("sqrt <a>")
+    .action((a) => {
 
-    console.log(Math.ceil(Number(a)))}
- )
+        console.log(Math.sqrt(Number(a)))
+    }
+    )
 program
-.command("sqrt <a>")
-.action((a)=>{
+    .command("exponential <base> <exponent>")
+    .action((base, exponent) => {
 
-    console.log(Math.sqrt(Number(a)))}
- )
+        console.log(Math.pow(Number(base), Number(exponent)))
+    }
+    )
+
+
 program
-.command("exponential <base> <exponent>")
-.action((base, exponent)=>{
+    .command("joke")
+    .description("Get a random joke")
+    .action(async () => {
+        try {
+            const res = await fetch("https://official-joke-api.appspot.com/random_joke");
+            const data = await res.json();
+            console.log(`${data.setup} \n... ${data.punchline}`);
+        } catch (err) {
+            console.error("Failed to fetch joke");
+        }
+    });
 
-    console.log(Math.pow(Number(base),Number(exponent)))}
- )
+program
+    .command("fact")
+    .description("Get a random interesting fact")
+    .action(async () => {
+        try {
+            const res = await fetch("https://uselessfacts.jsph.pl/api/v2/facts/random");
+            const data = await res.json();
+            console.log(`Fun Fact: ${data.text}`);
+        } catch (err) {
+            console.error("Failed to fetch fact");
+        }
+    });
 
+program
+    .command("advice")
+    .description("Get a random piece of advice")
+    .action(async () => {
+        try {
+            const res = await fetch("https://api.adviceslip.com/advice");
+            const data = await res.json();
+            console.log(`Advice: ${data.slip.advice}`);
+        } catch (err) {
+            console.error("Failed to fetch advice");
+        }
+    });
 
 program.parse()
-
